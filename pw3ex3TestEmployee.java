76bc3aa997523f685747ee5e2a6279924ab6c1fa
mariarimsmatti-netizen <mariarim_smatti@univ-biskra.dz>
2026-05-06 20:29:58 +0200

    Create pw3ex3TestEmployee.java

---
+++ pw3ex3TestEmployee.java
@@ -0,0 +1,11 @@
+package pw3ex3;
+
+public class TestEmployee {
+
+    public static void main(String[] args) {
+
+        Manager m = new Manager("Rami", 60000, 10000);
+
+        System.out.println(m);
+    }
+}
