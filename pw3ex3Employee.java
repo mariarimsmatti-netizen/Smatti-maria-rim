06abd27407075daca94c06e89db1bd03f6b4d033
mariarimsmatti-netizen <mariarim_smatti@univ-biskra.dz>
2026-05-06 20:28:35 +0200

    Create pw3ex3Employee.java

---
+++ pw3ex3Employee.java
@@ -0,0 +1,27 @@
+package pw3ex3;
+
+public class Employee {
+
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getSalary() {
+        return salary;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public String toString() {
+        return "Name: " + name + ", Salary: " + salary + " DA";
+    }
+}
