020789fb83cd0e46b9935a1bcebac6b74eef6f73
mariarimsmatti-netizen <mariarim_smatti@univ-biskra.dz>
2026-05-06 20:31:31 +0200

    Create pw3ex4Rectangle.java

---
+++ pw3ex4Rectangle.java
@@ -0,0 +1,21 @@
+package pw3ex4;
+
+
+public class Rectangle extends Shape {
+
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double area() {
+        return length * width;
+    }
+
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+}
